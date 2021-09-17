96e628653ad6bd00c981b1fb08dec2d54aee7f52
elodie baeza <elodie.baeza@gmail.com>
2021-09-17 17:03:10 +0200

    Create-React-App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from 'components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header"/>
+      {/* <Dashboard /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from 'assets/logo.png'
+import React from 'react'
+
+export default function Header(){
+    return(
+        <header>
+            <img src={logo} alt="" />
+        </header>
+    )
+}
