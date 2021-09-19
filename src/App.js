410f626a8be0e719885908ebf5dc6b989c08d1af
elodie baeza <elodie.baeza@gmail.com>
2021-09-19 19:35:00 +0200

    header nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from 'components/header/Header';
+import TransNav from 'components/transNav/TransNav';
 
 function App() {
   return (
     <div className="App">
       <Header className="App-header"/>
+      <TransNav />
       {/* <Dashboard /> */}
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
 import logo from 'assets/logo.png'
+import logoTitle from 'assets/logo_title.png'
 import React from 'react'
+import './Header.css'
 
 export default function Header(){
     return(
         <header>
-            <img src={logo} alt="" />
+            <a href="*" className='logo'>
+                <img className='logoIcon' src={logo} alt="logo" />
+                <img src={logoTitle} alt="logo" />
+            </a>
+            <nav>
+                <a href="*">Accueil</a>
+                <a href="*">Profil</a>
+                <a href="*">Réglage</a>
+                <a href="*">Communauté</a>
+            </nav>
         </header>
     )
 }
