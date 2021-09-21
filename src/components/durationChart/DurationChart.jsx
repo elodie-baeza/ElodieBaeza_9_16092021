9ec21f1aaab1ccc7930003e069d7fd27f755bf00
elodie baeza <elodie.baeza@gmail.com>
2021-09-22 01:23:44 +0200

    Add GroupedBarChart

---
+++ src/components/durationChart/DurationChart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './DurationChart.css'
+
+// ************* Line Chart ************** //
+
+export default function DurationChart(){
+    return(
+        <div></div>
+    )
+}
