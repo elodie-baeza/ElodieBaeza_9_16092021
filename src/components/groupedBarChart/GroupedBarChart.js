f3815cbd60831e33a6d3d1699da7edb3c7bc9f22
elodie baeza <elodie.baeza@gmail.com>
2021-10-11 12:26:08 +0200

    GroupedbarChart hotfix

--- src/components/groupedBarChart/GroupedBarChart.js
+++ src/components/groupedBarChart/GroupedBarChart.js
@@ -23,16 +23,16 @@ export default function GroupedBarChart({ data }) {
                         
             // Add X axis
             const x = d3.scaleBand()
-                .domain(data.map(d => d.day.slice(-2)))
-                .range([0, width - margin.right])
-                .padding([0.5])
-    
+                .domain(data.map(d => parseInt(d.day.slice(-2)),10))
+                .range([0, width - margin.right - margin.left])
+                .padding([0.4])
+                
             const xAxis = (g) =>
                 g.attr("transform", `translate(0, ${height-margin.bottom+15})`)
                 .call(d3.axisBottom(x));
             
             svg.select('.x-axis').call(xAxis)
-    
+            
             // Add Y axis for kilogram
             const y = d3.scaleLinear()
                 .domain([ (minWeight-1), (maxWeight+1) ])
@@ -43,11 +43,11 @@ export default function GroupedBarChart({ data }) {
                 .call(d3
                     .axisRight(y)
                     .ticks(Math.floor(maxWeight)-Math.floor(minWeight-1))
-                .tickSize(-width-margin.left-margin.right)
-                );
-
+                    .tickSize(- width + margin.left*2 + margin.right)
+                    );
+                
             svg.select('.y-axis').call(yAxis)
-    
+                
             // Add Y1 axis for calories
             const y1 = d3.scaleLinear()
                 .domain([ (minCalories-50), (maxCalories+50)])
@@ -57,76 +57,94 @@ export default function GroupedBarChart({ data }) {
             const xSubgroup = d3.scaleBand()
                 .domain(subgroups)
                 .range([0, x.bandwidth()])
-                .padding([0.4])
+                .padding([0.5])
     
             // console.dir(xSubgroup)
     
             // color palette = one color per subgroup
             const color = d3.scaleOrdinal()
                 .domain(subgroups)
-                .range(['#000000','#ff0000'])
-    
-            // Show the kilogram bars
+                .range(['#000000','#ff0000'])        
+            
+            // select divTooltip
+            var divTooltip = d3.select(".tooltip")
+
+            // Show bars
             svg.select(".plot-area")
-                .selectAll("g")
+                .selectAll(".groupBars")
                 // Enter in data = loop group per group
                     .data(data)
                     .join("g")
-                        .attr("transform", d => `translate(${x(d.day.slice(-2))}, 0)`)
-                .selectAll(".barA")
-                    .data(function(d) {
-                        return subgroups.map(function(key) {
-                            return {key: 'kilogram', value: d.kilogram}; 
-                        }); 
-                    })
-                    .join("rect")
-                        .attr('class', 'barA')
-                        .attr("x", d => xSubgroup(d.key))
-                        .attr("y", d => y(d.value))
-                        .attr("width", xSubgroup.bandwidth())
-                        .attr("height", d => height - margin.bottom - y(d.value))
-                        .attr("fill", d => color(d.key))
-    
-            // Show the calories bars
-            svg.select(".plot-area")
-                .selectAll("g")
-                // Enter in data = loop group per group
-                    .data(data)
-                    .join("g")
-                        .attr("transform", d => `translate(${x(d.day.slice(-2))}, 0)`)
-                .selectAll(".barB")
-                    .data(function(d) {
-                        return subgroups.map(function(key) {
-                            return {key: 'calories', value: d['calories']}; 
-                        }); 
-                    })
-                    .join("rect")
-                        .attr('class', 'barB')
-                        .attr("x", d => xSubgroup(d.key))
-                        .attr("y", d => y1(d.value))
-                        .attr("width", xSubgroup.bandwidth())
-                        .attr("height", d => height - margin.bottom - y1(d.value))
-                        .attr("fill", d => color(d.key))
-    
+                        .attr('class', 'groupBars')
+                        .attr("transform", d => `translate(${x(parseInt(d.day.slice(-2)),10)}, 0)`)
+                        .on("mouseover", function(event, d){
+                            divTooltip.style("opacity", 1)
+                                .style("left", (event.pageX) + "px")
+                                .style("top", (event.pageY - 28) + "px");
+                            divTooltip.html(`${d.kilogram} kg <br> ${d.calories} kCal`)
+                        })
+                        .on("mouseout", function(d) {
+                            divTooltip.style("opacity", 0);
+                            divTooltip.html('')
+                        })
+                    .selectAll(".bar")
+                        .data(d => subgroups.map(key => ({key, value: d[key]}) ))
+                        .join("rect")
+                            .attr('class', 'bar')
+                            .attr('rx', 3)
+                            .attr('ry', 3)
+                            .attr("x", d => xSubgroup(d.key))
+                            .attr("y", d => {
+                                if (d.key === 'kilogram')
+                                    return y(d.value);
+                                return y1(d.value)
+                            })
+                            .attr("width", xSubgroup.bandwidth())
+                            .attr("height", d => {
+                                if (d.key === 'kilogram')
+                                    return height - margin.bottom - y(d.value)
+                                return height - margin.bottom - y1(d.value)
+                            })
+                            .attr("fill", d => color(d.key))
+
+            //TOOLTIP AREA
+            svg.select('.plot-area')
+                .selectAll('.groupBars')
+                .append('rect')
+                    .attr('class', 'groupedRectArea')
+                    .attr('x', 10)
+                    .attr('y', margin.top)
+                    .attr('width', 40)
+                    .attr('height', 165)
+                    .attr('fill', '#C4C4C4')
+                    .attr('opacity', 0)
+                    // .on("mouseover", function(event, d){
+                    //     this.parentNode.style('opacity', 0.5)
+                    // })
+                    // .on("mouseout", function(d) {
+                    // })
+
+
+            // var groupedRectArea = d3.select(".groupedRectArea")
+
             //title
             svg.append("text")
                 .attr('class','title')
                 .text("Activité quotidienne")
                 .attr("x", 32)
                 .attr("y", 32)
-
     
             // Handmade legend
             // legend poids
             svg.append("circle")
                 .attr('class','legend')
-                .attr("cx",width-325)
+                .attr("cx", width-370)
                 .attr("cy",32)
                 .attr("r", 4)
                 .style("fill", "#000000")
             svg.append("text")
                 .attr('class','legend')
-                .attr("x", width-315)
+                .attr("x", width-360)
                 .attr("y", 32)
                 .text("Poids (kg)")
                 .style("font-size", "15px")
@@ -137,13 +155,13 @@ export default function GroupedBarChart({ data }) {
             //legend calories
             svg.append("circle")
                 .attr('class','legend')
-                .attr("cx",width-210)
+                .attr("cx", width-270)
                 .attr("cy",32)
                 .attr("r", 4)
                 .style("fill", "#ff0000")
             svg.append("text")
                 .attr('class','legend')
-                .attr("x", width-200)
+                .attr("x", width-260)
                 .attr("y", 32)
                 .text("Calories brûlées (kCal)")
                 .style("font-size", "15px")
@@ -158,13 +176,14 @@ export default function GroupedBarChart({ data }) {
                 style={{
                     height: 320,
                     width: 835,
-                    marginRight: "0px",
-                    marginLeft: "0px",
+                    marginRight: 0,
+                    marginLeft: 0,
                 }}>
                 <g className="plot-area" />
                 <g className="x-axis" />
                 <g className="y-axis" />
             </svg>
+            <div className={'tooltip'} />
         </div>
 
     );
