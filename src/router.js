c013d76ca285ba44f2ef17db107787b94e32aa96
elodie baeza <elodie.baeza@gmail.com>
2021-10-11 12:29:02 +0200

    Add jsdoc and better-docs

--- src/router.js
+++ src/router.js
@@ -2,6 +2,12 @@ import Dashboard from 'pages/dashboard/Dashboard';
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+/**
+ * @class
+ * Allows you to switch users with url endpoint
+ * only user/12 and user/18 are available
+ */
+
 class Router extends React.Component {
 
     render() {
